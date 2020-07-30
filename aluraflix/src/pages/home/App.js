54fe8038ca3c2a74b35d899d486ab19db3c17616
Kássio <kciosantana@gmail.com>
2020-07-30 00:00:10 -0300

    Ajustes

--- aluraflix/src/pages/home/App.js
+++ aluraflix/src/pages/home/App.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import Menu from '../../components/menu/index';
 import Footer from '../../components/Footer/index'
+import dadosIniciais from '../../data/dados_iniciais.json'
+import BannerMain from '../../components/BannerMain/index'
+import Carousel from '../../components/Carousel/index'
+// impor VideoFrame from '../../components/BannerMain/components/VideoFrame'
 
 function App() {
   return (
       <div>
         <Menu />
+        <BannerMain 
+          videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
+          url={dadosIniciais.categorias[0].videos[0].url}
+          videoDescription={"Eita porra"}
+        />
+        <Carousel 
+          ignoreFirstVideo
+          category={dadosIniciais.categorias[0]}
+        />
         <Footer />
       </div>
   );
