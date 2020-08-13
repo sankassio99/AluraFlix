ec96ac6d7b7d356c8fd30027787f033fb0d6d4de
Kássio <kciosantana@gmail.com>
2020-08-13 15:45:36 -0300

    add screenshots

--- src/repositories/categorias/index.js
+++ src/repositories/categorias/index.js
@@ -2,46 +2,38 @@ import config from '../config';
 
 const URL_CATEGORIES = `${config.URL_BACKEND_TOP}/categorias`;
 
-function getAll() {
-  return fetch(`${URL_CATEGORIES}`)
-    .then(async (respostaDoServidor) => {
-      if (respostaDoServidor.ok) {
-        const resposta = await respostaDoServidor.json();
-        return resposta;
-      }
-
-      throw new Error('Não foi possível pegar os dados :(');
-    });
+async function getAll() {
+  const respostaDoServidor = await fetch(`${URL_CATEGORIES}`);
+  if (respostaDoServidor.ok) {
+    const resposta = await respostaDoServidor.json();
+    return resposta;
+  }
+  throw new Error('Não foi possível pegar os dados :(');
 }
 
-function getAllWithVideos() {
-  return fetch(`${URL_CATEGORIES}?_embed=videos`)
-    .then(async (respostaDoServidor) => {
-      if (respostaDoServidor.ok) {
-        const resposta = await respostaDoServidor.json();
-        return resposta;
-      }
-
-      throw new Error('Não foi possível pegar os dados :(');
-    });
+async function getAllWithVideos() {
+  const respostaDoServidor = await fetch(`${URL_CATEGORIES}?_embed=videos`);
+  if (respostaDoServidor.ok) {
+    const resposta = await respostaDoServidor.json();
+    return resposta;
+  }
+  throw new Error('Não foi possível pegar os dados :(');
 }
 
-function createCategory(category) {
-  return fetch(`${URL_CATEGORIES}`, {
+async function createCategory(category) {
+  const respostaDoServidor = await fetch(`${URL_CATEGORIES}`, {
     method: 'POST',
     headers: {
       'Content-type': 'application/json',
     },
     body: JSON.stringify(category),
-  })
-    .then(async (respostaDoServidor) => {
-      if (respostaDoServidor.ok) {
-        const resposta = await respostaDoServidor.json();
-        return resposta;
-      }
-
-      throw new Error('Não foi possível cadastrar a caregoria :(');
-    });
+  });
+  if (respostaDoServidor.ok) {
+    const resposta = await respostaDoServidor.json();
+    console.log("Cadastrado com sucesso!");
+    return resposta;
+  }
+  throw new Error('Não foi possível cadastrar a caregoria :(');
 }
 
 export default {
